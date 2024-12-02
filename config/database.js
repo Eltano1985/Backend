6ba97023b78837eda39f74f399ec06c441b043e0
Eltano1985 <e.amoresano85@gmail.com>
2024-12-02 19:52:41 -0300

    modificaciones al inicio de conexion con base de datos

--- config/database.js
+++ config/database.js
@@ -8,7 +8,9 @@ const pool = createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 3306,
 });
+
+console.log(process.env.DB_PORT?"true":"false");
 
 export default pool;
